4faf29cde826c61c7fc3f50ed9882d06a4aba316
Luke Johnston <lwjohnst@gmail.com>
2020-08-27 19:58:51 +0200

    scrape physionet and mmash-page just in case

--- R/scrape-physionet.R
+++ R/scrape-physionet.R
@@ -1,6 +1,7 @@
 library(rvest)
 library(polite)
 library(rmarkdown)
+library(stringr)
 
 mmash_page <- bow("https://physionet.org/content/mmash/1.0.0/")
 
@@ -8,15 +9,20 @@ mmash_html <- mmash_page %>%
     scrape() %>%
     html_node(".col-md-8")
 
-mmash_html %>%
-    html_nodes(xpath = "//div[not(contains(@class,'alert'))]")
-
 temp_html <- tempfile(fileext = ".html")
 write_html(mmash_html, temp_html)
 
 temp_md <- tempfile(fileext = ".md")
 pandoc_convert(temp_html, to = "markdown_strict", output = temp_md)
 
-render(input = temp_md, output_file = here::here("resources/mmash-page.html"),
-       output_format = html_document(pandoc_args = c("--metadata", "title=MMASH Page")))
+readLines(temp_md)[-(8:76)] %>%
+    str_remove("\\\\") %>%
+    writeLines(temp_md)
 
+render(
+    input = temp_md,
+    output_file = here::here("resources/mmash-page.html"),
+    output_format = html_document(
+        pandoc_args = c("--metadata", "title=MMASH Page")
+    )
+)
