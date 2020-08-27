7ed7fe219e854ed3b0fbc517ebb441c6fcca07d6
Luke Johnston <lwjohnst@gmail.com>
2020-08-27 22:11:34 +0200

    move html page of mmash into public

--- _build.R
+++ _build.R
@@ -1,3 +1,8 @@
 
 # Create the course book
 bookdown::render_book('index.Rmd', 'bookdown::gitbook', quiet = TRUE)
+
+# Move mmash page html file into public
+fs::dir_create(here::here("public/resources/"))
+fs::file_copy(here::here("resources/mmash-page.html"),
+              here::here("public/resources/mmash-page.html"))
