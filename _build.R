178108fe72a97f3403e9f3d7a26875788133b134
Luke Johnston <lwjohnst@gmail.com>
2020-08-28 16:27:41 +0200

    move slides etc over on build

--- _build.R
+++ _build.R
@@ -2,7 +2,18 @@
 # Create the course book
 bookdown::render_book('index.Rmd', 'bookdown::gitbook', quiet = TRUE)
 
+# Create necessary folders in public
+fs::dir_create(here::here("public", c("resources", "slides")))
+
 # Move mmash page html file into public
-fs::dir_create(here::here("public/resources/"))
 fs::file_copy(here::here("resources/mmash-page.html"),
               here::here("public/resources/mmash-page.html"))
+
+slide_files <- fs::dir_ls("slides", glob = "*.html")
+fs::file_copy(slide_files, fs::path("public/", slide_files))
+
+slide_css <- fs::dir_ls("resources", glob = "*.css")
+fs::file_copy(slide_css, fs::path("public/", slide_css))
+
+logo_files <- fs::dir_ls("images", regexp = ".*logo.*")
+fs::file_copy(logo_files, fs::path("public/", logo_files))
