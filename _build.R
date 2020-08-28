5081234efbb6d99d0aeafec2c8bd77790e608185
Luke Johnston <lwjohnst@gmail.com>
2020-08-28 19:24:17 +0200

    don't need these in build

--- _build.R
+++ _build.R
@@ -1,19 +1,10 @@
 
+# bookdown::clean_book(TRUE)
 # Create the course book
 bookdown::render_book('index.Rmd', 'bookdown::gitbook', quiet = TRUE)
 
 # Create necessary folders in public
 fs::dir_create(here::here("public", c("resources", "slides")))
 
-# Move mmash page html file into public
-fs::file_copy(here::here("resources/mmash-page.html"),
-              here::here("public/resources/mmash-page.html"))
-
-slide_files <- fs::dir_ls("slides", glob = "*.html")
-fs::file_copy(slide_files, fs::path("public/", slide_files))
-
 slide_css <- fs::dir_ls("resources", glob = "*.css")
 fs::file_copy(slide_css, fs::path("public/", slide_css))
-
-logo_files <- fs::dir_ls("images", regexp = ".*logo.*")
-fs::file_copy(logo_files, fs::path("public/", logo_files))
