d11e2f7688d99d130ba67c66c8c4bd9724c03232
Luke Johnston <lwjohnst@gmail.com>
2020-08-30 22:52:27 +0200

    forgot to fix the new paths so the slide css files could be copied over

--- _build.R
+++ _build.R
@@ -4,7 +4,7 @@ bookdown::clean_book(TRUE)
 bookdown::render_book('index.Rmd', 'bookdown::gitbook', quiet = TRUE)
 
 # Create necessary folders in public
-fs::dir_create(here::here("public", c("resources", "slides")))
+fs::dir_create(here::here("public", c("resources", "slides", "includes")))
 
-slide_css <- fs::dir_ls("resources", glob = "*.css")
+slide_css <- fs::dir_ls("includes", glob = "*.css")
 fs::file_copy(slide_css, fs::path("public/", slide_css), overwrite = TRUE)
