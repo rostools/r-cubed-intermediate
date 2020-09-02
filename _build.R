1b2362da0c72d3c2ec866d776f827037c3f5688a
Luke Johnston <lwjohnst@gmail.com>
2020-09-02 17:29:00 +0200

    message to build and keep the render quiet

--- _build.R
+++ _build.R
@@ -1,11 +1,14 @@
 #!/usr/bin/env Rscript
 
+message("Starting to build the website.")
 bookdown::clean_book(TRUE)
 # Create the course book
-bookdown::render_book('index.Rmd', 'bookdown::gitbook')
+output <- bookdown::render_book('index.Rmd', 'bookdown::gitbook', quiet = TRUE)
 
 # Create necessary folders in public
 fs::dir_create(here::here("public", c("resources", "slides", "includes")))
 
 slide_css <- fs::dir_ls("includes", glob = "*.css")
 fs::file_copy(slide_css, fs::path("public/", slide_css), overwrite = TRUE)
+
+message("Finished building it.")
