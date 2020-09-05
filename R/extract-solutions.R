3e651c5a8d5183cf75abc34cec4a92f16cd8d052
Luke Johnston <lwjohnst@gmail.com>
2020-09-05 22:53:56 +0200

    function that extracts and can display solutions to exercises

--- R/extract-solutions.R
+++ R/extract-solutions.R
@@ -1,29 +1,26 @@
 
+#' Extract the Exercise header title and the solution code chunk.
+#'
+#' This function is used to move the code into the solution appendix
+#' of the website.
+#'
+#' @param rmd_file The R Markdown file to process.
+#'
+#' @return A character string.
+#'
 extract_solution_chunks <- function(rmd_file) {
-    # readLines(rmd_file) %>%
-    #     commonmark::markdown_xml() %>%
-    #     xml2::read_xml() %>%
-    #     xml2::xml_find_all("//*[@info[contains(., 'solution')]]")
     readLines(rmd_file) %>%
         commonmark::markdown_xml() %>%
-        xml2::read_xml()
-    # %>%
-    #     xml2::xml_find_all("")
-        # xml2::xml_find_all("//*[@info[contains(., 'solution')]]/preceding::*[heading]")
+        xml2::read_xml() %>%
+        xml2::xml_find_all(
+            "//*[@level = '2']/*[contains(text(), 'Exercise')] | //*[@info[contains(., 'solution')]]"
+        ) %>%
+        xml2::xml_text() %>%
+        purrr::map( ~ dplyr::if_else(
+            stringr::str_detect(.x, "^Exercise: "),
+            stringr::str_c("**", stringr::str_remove(.x, ' \\{.*\\}$'), "**"),
+            stringr::str_c("\n```r\n", .x, "```\n\n")
+        )) %>%
+        stringr::str_split("\n") %>%
+        unlist()
 }
-
-extract_solution_chunks("03-dry.Rmd") %>%
-    xml2::xml_find_all("//*[@level = '2']/*[contains(text(), 'Exercise')] | //*[@info[contains(., 'solution')]]") %>%
-    xml2::xml_text()
-
-readLines("03-dry.Rmd") %>%
-    commonmark::markdown_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes(xpath = "//code[contains(@class, 'language-{r')]") %>%
-    rvest::html_text()
-    rvest::html_nodes(xpath = "//h2[contains(text(), 'Exercise')] | //code[contains(., 'solution')]]"")
-
-rvest::read_html
-
-
-# knitr::purl("03-dry.Rmd")
