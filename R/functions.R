89742bfb4823670db652e6e582274aed6bf553aa
Luke W. Johnston <lwjohnst@gmail.com>
2021-06-08 22:29:01 +0200

    function to trim output for website

--- R/functions.R
+++ R/functions.R
@@ -114,3 +114,13 @@ source_session <- function(rmd_file) {
            print. = FALSE)
     return(invisible(NULL))
 }
+
+#' Trim file path for showing in book.
+#'
+#' @param data Data frame with `file_path_id` column.
+#'
+#' @return Tibble.
+#'
+trim_filepath_for_book <- function(data) {
+    mutate(data, file_path_id = gsub(".*\\/data-raw", "data-raw", file_path_id))
+}
