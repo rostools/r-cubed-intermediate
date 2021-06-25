59b1d22eb2176c8245dfd47b79b27e6b3a174794
Luke W. Johnston <lwjohnst@gmail.com>
2021-06-25 14:24:10 +0200

    fix path to css for slides

--- _build.R
+++ _build.R
@@ -7,9 +7,9 @@ bookdown::clean_book(TRUE)
 output <- bookdown::render_book('index.Rmd', 'bookdown::bs4_book', quiet = TRUE)
 
 # Create necessary folders in public
-fs::dir_create(here::here("public", c("resources", "slides", "includes")))
+fs::dir_create(here::here("public", c("resources", "slides")))
 
-slide_css <- fs::dir_ls("includes", glob = "*.css")
+slide_css <- fs::dir_ls("slides", glob = "*.css")
 fs::file_copy(slide_css, fs::path("public/", slide_css), overwrite = TRUE)
 
 html_files <- fs::dir_ls("public", glob = "*.html")
