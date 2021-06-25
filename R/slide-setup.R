397bbf54c8c764128c75e0284bee5106ad5f61e4
Luke W. Johnston <lwjohnst@gmail.com>
2021-06-25 14:20:32 +0200

    move xariganthemer into slides plus make slide text bigger

--- R/slide-setup.R
+++ R/slide-setup.R
@@ -20,10 +20,9 @@ knitr::opts_chunk$set(echo = FALSE)
 library(xaringanthemer)
 style_mono_accent(
     base_color = "#0d47a1",
-    base_font_size = "24px",
-    code_font_size = "1.2em",
+    text_font_size = "24px",
+    code_font_size = "1em",
     header_font_google = google_font("Cabin"),
     text_font_google = google_font("KoHo"),
-    code_font_google = google_font("Source Code Pro"),
-    outfile = here::here("includes/xaringan-themer.css")
+    code_font_google = google_font("Source Code Pro")
 )
