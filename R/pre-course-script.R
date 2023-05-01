9ef1250d913e24518999ddba534f03c8377c80ac
Luke W. Johnston <lwjohnst@gmail.com>
2023-05-01 16:42:00 +0200

    styler reformat

--- R/pre-course-script.R
+++ R/pre-course-script.R
@@ -6,14 +6,18 @@ download.file(mmash_link, destfile = here("data-raw/mmash-data.zip"))
 
 # Unzip
 unzip(here("data-raw/mmash-data.zip"),
-      exdir = here("data-raw"),
-      junkpaths = TRUE)
+  exdir = here("data-raw"),
+  junkpaths = TRUE
+)
 unzip(here("data-raw/MMASH.zip"),
-      exdir = here("data-raw"))
+  exdir = here("data-raw")
+)
 
 # Remove/tidy up left over files
 library(fs)
-file_delete(here(c("data-raw/MMASH.zip",
-                   "data-raw/SHA256SUMS.txt",
-                   "data-raw/LICENSE.txt")))
+file_delete(here(c(
+  "data-raw/MMASH.zip",
+  "data-raw/SHA256SUMS.txt",
+  "data-raw/LICENSE.txt"
+)))
 file_move(here("data-raw/DataPaper"), here("data-raw/mmash"))
