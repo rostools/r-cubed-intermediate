5038e2baa89338bb00232b247eb4e7598ec644a1
Luke W. Johnston <lwjohnst@gmail.com>
2025-03-07 15:19:56 +0100

    build: :recycle: use soft link of `_extensions` in slides to render them

--- _build-slides.sh
+++
@@ -1,9 +0,0 @@
-cp slides/introduction.qmd _introduction.qmd
-cp slides/what-next.qmd _what-next.qmd
-
-quarto render _*.qmd --to r3-theme-revealjs
-
-rm _what-next.qmd _introduction.qmd
-
-mv _introduction.html slides/introduction.html
-mv _what-next.html slides/what-next.html
