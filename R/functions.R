01cc89974abbe672fce93e35d4ea2d2f4982d3be
Luke W. Johnston <lwjohnst@gmail.com>
2026-04-08 12:30:15 +0200

    style: :art: formatted R code

--- R/functions.R
+++ R/functions.R
@@ -5,5 +5,8 @@
 #' @returns Tibble.
 #'
 trim_filepath_for_book <- function(data) {
-  mutate(data, file_path_id = gsub(".*\\/data-raw", "data-raw", file_path_id))
+  dplyr::mutate(
+    data,
+    file_path_id = gsub(".*\\/data-raw", "data-raw", file_path_id)
+  )
 }
